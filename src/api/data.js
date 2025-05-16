2c56b57d392125916214a7c3b347a471c8f1c26e
hadis <hadisbeigi97@gmail.com>
2025-05-17 02:14:55 +0330

    first commit

--- src/api/data.js
+++ src/api/data.js
@@ -3,31 +3,30 @@ import axios from "axios";
 export const getFilms = async (page = 1, perPage = 12, query = "") => {
   const queryParam = query ? `&q=${query}` : "";
   const { data } = await axios.get(
-    `https://moviesapi.codingfront.dev/api/v1/movies?page=${page}&per_page=${perPage}${queryParam}`
+    `${
+      import.meta.env.VITE_API_URL
+    }/movies?page=${page}&per_page=${perPage}${queryParam}`
   );
   return data;
 };
 
 export const getGenresName = async () => {
-  const { data } = await axios.get(
-    `https://moviesapi.codingfront.dev/api/v1/genres`
-  );
+  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/genres`);
   return data;
 };
 
 export const getGenreName = async (genre, page = 1) => {
   const { data } = await axios.get(
-    `https://moviesapi.codingfront.dev/api/v1/genres/${genre}/movies?page=${page}`
+    `${import.meta.env.VITE_API_URL}/genres/${genre}/movies?page=${page}`
   );
   return data;
 };
-
 export const getFilmDetails = async (id) => {
   if (!id) {
     return;
   }
   const { data } = await axios.get(
-    `https://moviesapi.codingfront.dev/api/v1/movies/${id}`
+    `${import.meta.env.VITE_API_URL}/movies/${id}`
   );
   return data;
 };
