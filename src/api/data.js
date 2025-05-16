6774d0858e8996f0751ae7834654f9aa5b4a218b
hadis <hadisbeigi97@gmail.com>
2025-05-16 23:37:36 +0330

    feat: اضافه کردن قابلیت جستجو و فیلتر ژانر به صفحه اصلی
    - اضافه شدن جستجوی فیلم و سریال
    - اضافه شدن فیلتر بر اساس ژانر
    - بهبود رابط کاربری کارت‌های نمایش فیلم

--- src/api/data.js
+++ src/api/data.js
@@ -7,3 +7,17 @@ export const getFilms = async (page = 1, perPage = 12, query = "") => {
   );
   return data;
 };
+
+export const getGenresName = async () => {
+  const { data } = await axios.get(
+    `https://moviesapi.codingfront.dev/api/v1/genres`
+  );
+  return data;
+};
+
+export const getGenreName = async (genre, page = 1) => {
+  const { data } = await axios.get(
+    `https://moviesapi.codingfront.dev/api/v1/genres/${genre}/movies?page=${page}`
+  );
+  return data;
+};
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,12 +9,13 @@ export async function loader() {
   const response = await getFilms(1, 12);
   const movies = response.data;
   const metadata = response.metadata;
+  const genres = await getGenresName();
 
-  return { movies, metadata };
+  return { movies, metadata, genres };
 }
 
 export default function Home() {
-  const { movies: initialMovies, metadata } = useLoaderData();
+  const { movies: initialMovies, metadata, genres } = useLoaderData();
   const [movies, setMovies] = useState(initialMovies);
   const [page, setPage] = useState(2);
   const [hasMore, setHasMore] = useState(true);
@@ -22,6 +23,17 @@ export default function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchQuery = searchParams.get("q") || "";
   const [filteredMovies, setFilteredMovies] = useState([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState("All");
+  const [genreMovies, setGenreMovies] = useState([]);
+  const [genrePage, setGenrePage] = useState(1);
+  const [hasMoreGenre, setHasMoreGenre] = useState(true);
+  const [totalMoviesCount, setTotalMoviesCount] = useState(
+    metadata.total_count
+  );
+  const [genreTotalCount, setGenreTotalCount] = useState(0);
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [isGenreLoading, setIsGenreLoading] = useState(false);
 
   const loadMoreMovies = async () => {
     setLoading(true);
@@ -67,8 +79,76 @@ export default function Home() {
     fetchSearchedMovies();
   }, [searchQuery]);
 
+  const filterByGenre = async (genreName) => {
+    setSelectedGenre(genreName);
+    setSearchParams({});
+    setGenrePage(1);
+    setHasMoreGenre(true);
+    setIsGenreLoading(true);
+
+    if (genreName === "All") {
+      setFilteredMovies([]);
+      setTotalMoviesCount(metadata.total_count);
+      setIsGenreLoading(false);
+    } else {
+      try {
+        const response = await getGenreName(genreName, 1);
+        setGenreMovies(response.data);
+        setGenreTotalCount(response.metadata.total_count);
+      } catch (error) {
+        console.error("Error fetching genre movies:", error);
+        setGenreMovies([]);
+        setHasMoreGenre(false);
+        setGenreTotalCount(0);
+      } finally {
+        setIsGenreLoading(false);
+      }
+    }
+  };
+
+  const loadMoreGenreMovies = async () => {
+    try {
+      const nextPage = genrePage + 1;
+      const response = await getGenreName(selectedGenre, nextPage);
+      const moreMovies = response.data;
+
+      setGenreMovies((prev) => [...prev, ...moreMovies]);
+      setGenrePage(nextPage);
+
+      if (moreMovies.length === 0) {
+        setHasMoreGenre(false);
+      }
+    } catch (error) {
+      console.error("Failed to load more genre movies:", error);
+      setHasMoreGenre(false);
+    }
+  };
+
+  const visibleGenres = genres.slice(0, 4);
+  const hiddenGenres = genres.slice(4);
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      setInitialLoading(false);
+    }
+  }, [movies]);
   return (
     <section>
+      <div className="w-5/12 mt-20 mb-6  ltr:ml-32 rtl:mr-32">
+        <h1 className="font-semibold font-Poppins text-Grey50 text-custom-64 mb-4">
+          {t("appTitle")}
+        </h1>
+        <p className="font-Poppins font-normal text-base text-Grey300 2xl:w-3/4">
+          List of movies and TV Shows, I,
+          <span className="text-Primary400">Pramod Poudel </span> Pramod Poudel
+          have watched till date. Explore what I have watched and also feel free
+          to make a suggestion.😉
+        </p>
+      </div>
+
       <form className="max-w-md mb-20 ltr:ml-32 rtl:mr-32 border-gray-500">
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -95,49 +175,159 @@ export default function Home() {
           />
         </div>
       </form>
-      <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => (
-            <div
-              key={movie.id}
-              className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
-            >
-              <HomeCard movie={movie} />
-            </div>
-          ))
-        ) : (
-          <p className="text-Grey300 text-center w-full mt-4">
-            "No results found."
-          </p>
+
+      <div className="inline-flex ltr:ml-32 rtl:mr-32 mb-6 p-2 bg-Grey700 rounded-xl relative flex-wrap gap-2">
+        <button
+          onClick={() => filterByGenre("All")}
+          className={`inline-flex items-center rounded-lg px-6 py-2 text-sm font-Poppins ${
+            selectedGenre === "All"
+              ? "bg-Primary400 text-white"
+              : "bg-transparent text-Grey300 hover:bg-Grey800"
+          }`}
+        >
+          All
+        </button>
+
+        {visibleGenres.map((genre) => (
+          <button
+            key={genre.name}
+            onClick={() => filterByGenre(genre.name)}
+            className={`inline-flex items-center rounded-lg px-6 py-2 text-sm font-Poppins ${
+              selectedGenre === genre.name
+                ? "bg-Primary400 text-white"
+                : "bg-transparent text-Grey300 hover:bg-Grey800"
+            }`}
+          >
+            {genre.name}
+          </button>
+        ))}
+
+        <button
+          onClick={toggleDropdown}
+          className="inline-flex items-center rounded-lg px-6 py-2 text-sm font-Poppins bg-transparent text-Grey300 hover:bg-Grey800"
+        >
+          "More"
+        </button>
+
+        {isDropdownOpen && (
+          <div className="absolute top-full mt-2 z-10 grid w-max grid-cols-2 bg-white rounded-lg shadow-md p-4 dark:bg-gray-700">
+            {hiddenGenres.map((genre) => (
+              <button
+                key={genre.name}
+                onClick={() => {
+                  filterByGenre(genre.name);
+                  setIsDropdownOpen(false);
+                }}
+                className="text-left text-sm text-gray-700 dark:text-white hover:text-blue-600 dark:hover:text-blue-400 py-1 px-2"
+              >
+                {genre.name}
+              </button>
+            ))}
+          </div>
         )}
-      </div>{" "}
-      <InfiniteScroll
-        dataLength={movies.length}
-        next={loadMoreMovies}
-        hasMore={hasMore}
-        loader={
-          <div className="flex justify-center py-4">
-            <Spinner color="purple" size="lg" />
-          </div>
-        }
-        endMessage={
-          <div className="flex justify-center py-4">
-            No more movies available.
-          </div>
-        }
-        scrollThreshold={0.9}
-      >
-        <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
-          {movies.map((movie) => (
+      </div>
+
+      <h3 className="text-Grey400 text-3xl font-semibold ltr:ml-32 rtl:mr-32 mb-6 font-Poppins">
+        {t("All")}
+        <span className="text-Grey400 font-normal text-base">
+          (
+          {searchQuery
+            ? filteredMovies.length
+            : selectedGenre === "All"
+            ? totalMoviesCount
+            : genreTotalCount}
+          )
+        </span>
+      </h3>
+
+      {initialLoading ? (
+        <div className="flex flex-wrap justify-center gap-4 mt-12">
+          {[...Array(12)].map((_, index) => (
             <div
-              key={movie.id}
-              className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
-            >
-              <HomeCard movie={movie} />
-            </div>
+              key={index}
+              className="w-1/3 md:w-1/4 lg:w-1/5 p-2 animate-pulse bg-Grey700 rounded-lg h-64"
+            ></div>
           ))}
         </div>
-      </InfiniteScroll>
+      ) : isGenreLoading ? (
+        <div className="flex justify-center py-10">
+          <Spinner color="purple" size="lg" />
+        </div>
+      ) : searchQuery ? (
+        <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
+          {filteredMovies.length > 0 ? (
+            filteredMovies.map((movie) => (
+              <div
+                key={movie.id}
+                className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
+              >
+                <HomeCard movie={movie} />
+              </div>
+            ))
+          ) : (
+            <p className="text-Grey300 text-center w-full mt-4">
+              {t("no_results")}
+            </p>
+          )}
+        </div>
+      ) : selectedGenre !== "All" ? (
+        <InfiniteScroll
+          dataLength={genreMovies.length}
+          next={loadMoreGenreMovies}
+          hasMore={hasMoreGenre}
+          loader={
+            <div className="flex justify-center items-center gap-2 py-4">
+              <span className="text-Grey300">{t("loading")}</span>
+              <Spinner color="purple" size="lg" />
+            </div>
+          }
+          endMessage={
+            <div className="flex justify-center py-4">
+              {t("no_more_movies_genre")}
+            </div>
+          }
+          scrollThreshold={0.9}
+        >
+          <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
+            {genreMovies.map((movie) => (
+              <div
+                key={movie.id}
+                className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
+              >
+                <HomeCard movie={movie} />
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
+      ) : (
+        <InfiniteScroll
+          dataLength={movies.length}
+          next={loadMoreMovies}
+          hasMore={hasMore}
+          loader={
+            <div className="flex justify-center py-4">
+              <Spinner color="purple" size="lg" />
+            </div>
+          }
+          endMessage={
+            <div className="flex justify-center py-4">
+              {t("no_more_movies")}
+            </div>
+          }
+          scrollThreshold={0.9}
+        >
+          <div className="flex flex-wrap justify-center gap-x-6 gap-y-5 w-full">
+            {movies.map((movie) => (
+              <div
+                key={movie.id}
+                className="flex-none w-1/3 md:w-1/4 lg:w-1/5 p-2"
+              >
+                <HomeCard movie={movie} />
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
+      )}
     </section>
   );
 }
